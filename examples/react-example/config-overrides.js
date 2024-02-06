b0186b7845754079f6a0e1e5c268b76a78f136f7
Salavat Gainetdinov <salavat.gaynetdinov@meshconnect.com>
2024-02-06 14:17:17 +0500

    SEC-23 SEC-24 SEC-25 SEC-26 SEC-27 update packages, fix security issues

--- examples/react-example/config-overrides.js
+++ examples/react-example/config-overrides.js
@@ -1,12 +1,12 @@
-const path = require("path");
-RegExp.prototype.toJSON = RegExp.prototype.toString;
+const path = require('path')
+RegExp.prototype.toJSON = RegExp.prototype.toString
 module.exports = function override(config, env) {
-  const rulesOneOf = config.module.rules[1].oneOf;
+  const rulesOneOf = config.module.rules[1].oneOf
 
   rulesOneOf.splice(rulesOneOf.length - 1, 0, {
-    ...rulesOneOf[2],
-    include: path.join(__dirname, "../../packages/"),
-  });
+    ...rulesOneOf[3],
+    include: path.join(__dirname, '../../packages/')
+  })
 
-  return config;
-};
+  return config
+}
